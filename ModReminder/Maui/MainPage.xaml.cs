cb9c0702c90be6d12929cde74b9456ca95dcf47d
Patrick <patr7500@gmail.com>
2022-07-07 02:22:17 +0200

    Add project files.

---
+++ ModReminder/Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ModReminder.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ModReminder/Maui/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using ModReminder.Components.Data;
+
+namespace ModReminder.Maui
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var baseUri = "https://localhost:7208";
+
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUri) });
+
+            return builder.Build();
+        }
+    }
+}
